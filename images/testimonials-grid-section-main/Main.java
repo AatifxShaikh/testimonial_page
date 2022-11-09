1531460499379fd0d9f1bf49b03d5603dd0ebeb1
AatifxShaikh <96722552+AatifxShaikh@users.noreply.github.com>
2022-11-09 08:45:10 +0530

    Add files via upload

---
+++ images/testimonials-grid-section-main/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    static int logic(int x, int y) {
+        int z;
+        if (x > y) {
+            z = x + y;
+        } else {
+            z = (x + y) + 5;
+        }
+    }
+
+    public static void main(String[] args) {
+        int c = logic(10, 20);
+        System.out.println(c);
+    }
+
+}
